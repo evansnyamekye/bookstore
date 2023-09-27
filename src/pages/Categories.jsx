e3d8a9197f7d1b624fff8561497ef94bd94fd1c9
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-09-27 03:20:44 +0000

    Modified card and categories.jsx

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,5 +1,5 @@
 function Categories() {
-  return <div className="text-3xl h-screen font-extrabold font-robotoSlab text-center">This page is under construction</div>;
+  return <div className="text-3xl h-screen font-extrabold font-robotoSlab text-center">Under construction</div>;
 }
 
 export default Categories;
